93f70e669975fd042fcc45bfb70755b085873940
Nao Aoyama <ea80002@gmail.com>
2023-06-04 22:15:59 -0700

    css is changed

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
-const items = document.querySelectorAll(".img-item");
-console.log(items);
-
-
-
-const showFlowers = (e)=>{
-  for(let i = 0; i < items.length; i++){
-    const keyframes = {
-      opacity: [0,1],
-      rotate: ['5deg', 0],
-      scale: [1.1, 1],
-    }
-    const options = {
-      duration: 600,
-      delay: i * 300,
-      fill: 'forwards',
-    };
-    items[i].animate(keyframes, options);
-  }
-};
-
-const flowersObserver = new IntersectionObserver(showFlowers);
-
-flowersObserver.observe(document.querySelector(".gallery"));
+c
 
 
 
