d4ddaf79b8899d152c41bcbf8985c683dc764742
Nao Aoyama <ea80002@gmail.com>
2023-06-08 23:17:14 -0700

    css is changed10

--- php/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;charset=utf8mb4', 'root', '1536971');
-}   catch (PDOException $e) {
-    echo "データベース接続エラー：".$e->getMessage();
-}
-?>
